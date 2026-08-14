eb45c25c365ff01eb232a5da798a30b418587e0e
indresh-007 <indresh209721@gmail.com>
2026-08-14 23:07:30 +0530

    Type Annotations and Inference

---
+++ src/typesInTs.ts
@@ -0,0 +1,10 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "ChaiaurCode"
+
+let chaiFalvour:string = "masala chai"
+chaiFalvour = "Ginger tea"
+
+let chaiOrder:boolean
